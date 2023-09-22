1b23b52822527dfdd68c226f8b7c82c87c522e53
NicolasCeronM <106988078+NicolasCeronM@users.noreply.github.com>
2023-09-21 23:14:44 -0300

    añadido detalle de juego

--- gestionProducto/urls.py
+++ gestionProducto/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.juegos, name='juegos')
+    path('',views.juegos, name='juegos'),
+    path('detalle_juego/<int:producto_id>',views.detalle_juego, name='detalle_juego'),
 ]
--- gestionProducto/views.py
+++ gestionProducto/views.py
@@ -7,7 +7,17 @@ def juegos (request):
 
     juegos = Producto.objects.all()
 
-    ctx={
+    data={
         'juegos':juegos
     } 
-    return render(request,'juegos.html', ctx)
+    return render(request,'juegos.html', data)
+
+def detalle_juego(request,producto_id):
+
+    juego = Producto.objects.filter(id=producto_id)
+
+
+    data ={
+        'juego':juego
+    }
+    return render(request,'detalle_juego.html', data)
